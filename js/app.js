042da043bffd5df7c942171ffaaa3b736909c0e9
Mnyoki <mosesmunyoki6@gmail.com>
2023-06-17 04:26:13 +0300

    clickable projects images

--- js/app.js
+++ js/app.js
@@ -7,3 +7,35 @@ $(document).ready(function ()
     });
   
 })
+
+function proj1() {
+  window.location = "https://vigorous-bardeen-465c22.netlify.app/"
+}
+
+function proj2() {
+  window.location = "https://mnyoki.github.io/team-colab/"
+}
+
+function proj3() {
+  window.location = "https://mnyoki.github.io/cat-family/"
+}
+
+function proj4() {
+  window.location = "https://medie.netlify.app/"
+}
+
+function proj5() {
+  window.location = "https://mnyoki.github.io/grid-test/"
+}
+
+function proj6() {
+  window.location = "https://mnyoki.github.io/wombat-coffee/"
+}
+
+function proj7() {
+  window.location = "https://friendly-einstein-fc98b5.netlify.app/"
+}
+
+function proj8() {
+  window.location = "https://github.com/Mnyoki/login-template"
+}
