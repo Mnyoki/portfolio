0436b4a156ba0123191c2bcc28cceffcdb8e73b1
Mnyoki <mosesmunyoki6@gmail.com>
2023-06-17 03:01:05 +0300

    homepage animation div

--- js/app.js
+++ js/app.js
@@ -7,18 +7,3 @@ $(document).ready(function ()
     });
   
 })
-
-function myhref(web) {
-  window.location.href = web;
-}
-
-let tag_txt = document.getElementById("tag");
-
-const words = ["Web Developer", "Software Engineer"];
-let index = 0;
-
-setInterval(() => {
-  //console.log(words[index]);
-  tag_txt.innerHTML = words[index];
-  index = (index + 1) % words.length;
-}, 2000);
---
+++ js/home.js
@@ -0,0 +1,14 @@
+function myhref(web) {
+  window.location.href = web;
+}
+
+let tag_txt = document.getElementById("tag");
+
+const words = ["Web Developer", "Software Engineer"];
+let index = 0;
+
+setInterval(() => {
+  //console.log(words[index]);
+  tag_txt.innerHTML = words[index];
+  index = (index + 1) % words.length;
+}, 2000);
